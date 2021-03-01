75f1266ce0bf546185001b95014fceddefe8faa5
hgkPhDt <philiptorres.contato@gmail.com>
2021-03-01 10:45:09 -0300

    01/02/2021

--- OBESO.c/OBESO.c
+++ OBESO.c/OBESO.c
@@ -7,41 +7,45 @@
 #define LIMITE 30
 
 int main() {
-
-    float peso, altura, imc;
-	char convert;
+  	float peso, altura, imc;
+  	char convert;
 	bool op;
 
 	do{
-		system("clear");
-	
-		printf(" Ex: Peso = 70.5 e Altura = 1.75. \n");
-    	printf(" Qual o seu peso? ");
-    	scanf("%f", &peso);
-    	printf(" Qual a sua altura? ");
-	    scanf("%f", &altura);
-	    
-	    imc = peso/pow(altura,2);
-	    
-	    printf("\n >> Seu i.m.c. e: {%.1f} <<\n\n", imc);
-	    
-	    if(imc <= LIMITE){
-	    	printf(" Voce nao esta obeso!\n\n");
-	    }else{
-	    	printf(" Voce esta obeso!\n\n");
-		}
-    	
-    	printf(" Digite [E] para sair.\n Se desejar continuar, digite [C].\n >> ");
-    	scanf(" %c", &convert);
-    
-    	if(toupper(convert) == 'C'){
-    		op = true;
-    	}else{
-    		op = false;
-    		printf("Isso e um adeus?\n");
-    	}
+    system("clear");
 
+    //Obtendo os dados do usuário.
+    printf(" Ex: Peso = 70.5 e Altura = 1.75. \n");
+    printf(" Qual o seu peso? ");
+    scanf("%f", &peso);
+    printf(" Qual a sua altura? ");
+    scanf("%f", &altura);
+
+	//O I.M.C. é calculado dividindo o peso (em kg) pela altura ao quadrado (em metros).
+    imc = peso/pow(altura,2);
+	  
+    //Informa ao usuário o seu Índice de Massa Corporal.
+    printf("\n >> Seu i.m.c. e: {%.1f} <<\n\n", imc);
+	    
+    //Condicional para exibir se está obeso ou não.
+    if(imc <= LIMITE){
+      printf(" Voce nao esta obeso!\n\n");
+    }else{
+      printf(" Voce esta obeso!\n\n");
+    }
+    	
+    //Sistema para fazer o loop do software.
+    printf(" Digite [E] para sair.\n Se desejar continuar, digite [C].\n >> ");
+    scanf(" %c", &convert);
+  
+    if(toupper(convert) == 'C'){
+      op = true;
+    }else{
+      op = false;
+      printf(" Isso e um adeus?\n");
+    }
+    
     }while(op);
 
-    return 0;
+  return 0;
 }
